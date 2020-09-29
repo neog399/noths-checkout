26b60ba340640a71b544e12b58faa433a76c4d52
Gabor Neorcsity <neog399@gmail.com>
2020-09-29 20:07:22 +0100

    Implemented basic checkout (no discounts)

--- src/main/java/com/noths/checkout/Checkout.java
+++ src/main/java/com/noths/checkout/Checkout.java
@@ -1,12 +1,19 @@
 package com.noths.checkout;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Checkout {
-    public void scan(Product product) {
-        // TODO: implement
+    // Assumption is the Checkout class will be used by 1 thread at a time
+    // If multi-threaded access is needed then we either need to make this stateless (i.e. pass the whole item list)
+    // Or make this list thread safe (i.e. synchronize it or use an implementation that locks internally)
+    private final List<Product> items = new ArrayList<>();
+
+    public void scan(Product item) {
+        items.add(item);
     }
 
     public double total() {
-        // TODO: implement
-        return 0.0;
+        return items.stream().mapToDouble(Product::getPrice).sum();
     }
 }
