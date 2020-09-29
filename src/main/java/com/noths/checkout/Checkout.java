c92c79b7228d78b01f3d473bab75d27f1ef5b532
Gabor Neorcsity <neog399@gmail.com>
2020-09-29 21:16:58 +0100

    Implemented advanced checkout (with discounts)

--- src/main/java/com/noths/checkout/Checkout.java
+++ src/main/java/com/noths/checkout/Checkout.java
@@ -1,16 +1,21 @@
 package com.noths.checkout;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 public class Checkout {
+    // The order in which discounts are applied *is* important here, hence using List
+    private final List<Discount> discounts;
+
     // Assumption is the Checkout class will be used by 1 thread at a time
     // If multi-threaded access is needed then we either need to make this stateless (i.e. pass the whole item list)
     // Or make this list thread safe (i.e. synchronize it or use an implementation that locks internally)
     private final List<Product> items = new ArrayList<>();
 
     public Checkout(Discount... discounts) {
-        // TODO: implement
+        this.discounts = Arrays.asList(discounts);
     }
 
     public void scan(Product item) {
@@ -18,6 +23,10 @@ public class Checkout {
     }
 
     public double total() {
-        return items.stream().mapToDouble(Product::getPrice).sum();
+        double total = items.stream().mapToDouble(Product::getPrice).sum();
+        for (Discount discount : discounts) {
+            total -= discount.apply(items, total);
+        }
+        return total;
     }
 }
