3073dad2acd1adcc5419eb184dbe0cdb180bb801
Gabor Neorcsity <neog399@gmail.com>
2020-09-29 20:47:49 +0100

    Implemented '2+ travel card holder' discount

--- src/main/java/com/noths/checkout/TwoOrMoreTravelCardHolderDiscount.java
+++ src/main/java/com/noths/checkout/TwoOrMoreTravelCardHolderDiscount.java
@@ -3,9 +3,19 @@ package com.noths.checkout;
 import java.util.List;
 
 public class TwoOrMoreTravelCardHolderDiscount implements Discount {
+    // Make it configurable (via ctor parameter) if needed
+    // You could also make the product type and quantity configurable
+    private static final double REDUCED_TRAVEL_CARD_HOLDER_PRICE = 8.5;
+
     @Override
     public double apply(List<Product> items) {
-        // TODO: Implement
-        return -1.0;
+        long numOfTravelCardHolders = items.stream()
+                .filter(item -> item.getCode().equals(Product.TRAVEL_CARD_HOLDER.getCode()))
+                .count();
+        if (numOfTravelCardHolders < 2) {
+            return 0.0;
+        }
+
+        return numOfTravelCardHolders * (Product.TRAVEL_CARD_HOLDER.getPrice() - REDUCED_TRAVEL_CARD_HOLDER_PRICE);
     }
 }
