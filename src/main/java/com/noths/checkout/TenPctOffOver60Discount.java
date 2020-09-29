ce99e74e3827e198bfb4f175ab1bd8eae4fe582f
Gabor Neorcsity <neog399@gmail.com>
2020-09-29 20:30:51 +0100

    Implemented '10% off over 60' discount

--- src/main/java/com/noths/checkout/TenPctOffOver60Discount.java
+++ src/main/java/com/noths/checkout/TenPctOffOver60Discount.java
@@ -5,7 +5,12 @@ import java.util.List;
 public class TenPctOffOver60Discount implements Discount {
     @Override
     public double apply(List<Product> items) {
-        // TODO: Implement
-        return -1.0;
+        // Same piece of code as in Checkout. Pass subtotal to Discount if this keeps coming up to avoid duplication.
+        double subtotal = items.stream().mapToDouble(Product::getPrice).sum();
+        if (subtotal <= 60.0) {
+            return 0.0;
+        }
+
+        return subtotal / 10;
     }
 }
